ccabc785de0f00a14efa435e7c584433ae128a45
nivedhanr <nivedharenahan@gmail.com>
2022-12-05 15:37:48 +0530

    first

---
+++ src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.example.employeeapp_backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @GetMapping("/")
+    public String Welcomepage(){
+        return "Welcome to employeepage";
+    }
+
+
+}
