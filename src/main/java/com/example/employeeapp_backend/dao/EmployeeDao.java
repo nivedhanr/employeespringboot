58786bcedc6d26fd7b2895bb758fe4aad74f3a5b
nivedhanr <nivedharenahan@gmail.com>
2022-12-07 16:09:55 +0530

    nil

--- src/main/java/com/example/employeeapp_backend/dao/EmployeeDao.java
+++ src/main/java/com/example/employeeapp_backend/dao/EmployeeDao.java
@@ -1,7 +1,13 @@
 package com.example.employeeapp_backend.dao;
 
 import com.example.employeeapp_backend.Model.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employee,Integer> {
+    @Query(value = "SELECT `id`, `company`, `designation`, `empcode`, `mobile`, `name`, `password`, `salary`, `username` FROM `employee` WHERE empcode=:empcode",nativeQuery = true)
+    List<Employee> Search(@Param("empcode") Integer empcode);
 }
